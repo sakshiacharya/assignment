eae621dfbe930b73a2793cc07632aeb4aa100bfa
Sakshi Acharya <sakshiacharya48@gmail.com>
2023-06-20 13:04:16 +0530

    update

--- src/components/StoreName.jsx
+++ src/components/StoreName.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Box, TextField, Button } from '@mui/material';
+import image from '../Images/Image icon.png'
 
 export const StoreName = () => {
     return (
@@ -12,9 +14,44 @@ export const StoreName = () => {
                 <section className='container'>
                     <div className='store-div' >
                         <article>
-                        <h2 className='store-head'>Name Your Store</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus nulla eget adipiscing faucibus platea. Eu ultrices ipsum</p>
+                            <h2 className='store-head'>Name Your Store</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lacus nulla eget adipiscing faucibus platea. Eu ultrices ipsum</p>
                         </article>
+                    </div>
+
+                    <div className='store-div2' >
+                        <div className='border-box'>
+                            <div className='store-name'>
+                                Store Name
+                                <article>
+                                    <a>Home</a>
+                                    <a>About</a>
+                                    <a>Career</a>
+                                    <a>Contact</a>
+                                </article>
+                            </div>
+                            <Box
+                                sx={{
+                                    marginTop: '15px',
+                                    width: 600, 
+                                    // backgroundColor: '#606060',
+                                    // opacity: [0.9, 0.8, 0.7],
+ 
+
+                                }}
+                            >
+                                <img
+                                    src={image}
+                                    srcSet={image}
+                                    alt= ''
+                                    loading="lazy"
+                                    width= '600px'
+                                    height= '200px'
+                                />
+                            </Box>
+                            <p className='category'>Categories</p>
+
+                        </div>
                     </div>
                 </section>
             </div>
